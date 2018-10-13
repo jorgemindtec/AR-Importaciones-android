f08666050f2bc87bc1b8f1d5eda08a6d0021f2c5
jorgemindtec <jorgearellano@mindtec.com.mx>
2018-10-12 18:21:38 -0600

    Update lista-producto.js

--- www/js/lista-producto.js
+++ www/js/lista-producto.js
@@ -49,12 +49,13 @@ function ListaEbay(producto){
 	var precio = $("#"+id+" .s-item__price span").html();
 	var url = $("#"+id+" .link span").html();	
 	$("#productos-ebay").append("<div id='item-"+id+"' class='lista-productos-seleccionados'><div style='text-align:right;position: relative;'><img class='eliminarlista producto' src='img/eliminar.png' onclick='QuitarListaEbay("+producto+")'></div><div class='s-item__image-section'>"+imagen+"</div><div class='s-item__info clearfix'><h3 class='s-item__title'>"+titulo+"</h3><span>"+precio+"</span><div class='link' style='display:none;'>"+url+"</div></div><div class='cantidad-producto'><label>Cantidad: </label><input type='number' id='cantidad-"+id+"' min='1' max='999' step='1' value='1'></div></div>");	
+	var img = $("#"+id+" .s-item__image-section img").attr('src');
 	
 	var f = new Date();
 	var Fecha = f.getDate() + "-" + (f.getMonth() +1) + "-" + f.getFullYear();
 	db.transaction(function(tx){
 		tx.executeSql('INSERT INTO ListaBD(Usuario,Nombre,Telefono,Email,Mensaje,Tienda,IdProducto,Producto,Imagen,Precio,Enlace,Cantidad,Fecha)VALUES (?,?,?,?,?,?,?,?,?,?,?,?,?)',
-					["","","","","","Ebay",id,titulo,"",precio,url,"",Fecha],nullHandler,errorHandler);
+					["","","","","","Ebay",id,titulo,img,precio,url,1,Fecha],nullHandler,errorHandler);
 	});
 }
 function QuitarListaEbay(producto){
@@ -120,12 +121,13 @@ function ListaAmazon(producto){
 	var precio = $("#"+id+" .a-size-small.a-color-price.a-text-bold").html();
 	var url = $("#"+id+" .link span").html();			
 	$("#productos-amazon").append("<div id='item-"+id+"' class='lista-productos-seleccionados'><div style='text-align:right;position: relative;'><img class='eliminarlista producto' src='img/eliminar.png' onclick='QuitarListaAmazon("+producto+")'></div><div class='sx-table-pic'>"+imagen+"</div><div class='sx-table-detail'><h5 class='sx-title sx-title-inline'>"+titulo+"</h5><span class='price_am'>"+precio+"</span><div class='link' style='display:none;'>"+url+"</div></div><div class='cantidad-producto'><label>Cantidad: </label><input type='number' id='cantidad-"+id+"' min='1' max='999' step='1' value='1'></div></div>");
+	var img = $("#"+id+" .sx-table-pic img").attr('src');
 	
 	var f = new Date();
 	var Fecha = f.getDate() + "-" + (f.getMonth() +1) + "-" + f.getFullYear();
 	db.transaction(function(tx){
 		tx.executeSql('INSERT INTO ListaBD(Usuario,Nombre,Telefono,Email,Mensaje,Tienda,IdProducto,Producto,Imagen,Precio,Enlace,Cantidad,Fecha)VALUES (?,?,?,?,?,?,?,?,?,?,?,?,?)',
-					["","","","","","Amazon",id,titulo,"",precio,url,"",Fecha],nullHandler,errorHandler);
+					["","","","","","Amazon",id,titulo,img,precio,url,"",Fecha],nullHandler,errorHandler);
 	});		
 	/*$("#productos-amazon").html("");
 	jQuery.each( AmazonList, function( i, valor ) {
@@ -205,12 +207,13 @@ function ListaAlibaba(producto){
 	var precio = $("#"+id+" .product-price").html();
 	var url = $("#"+id+" .link span").html();			
 	$("#productos-alibaba").append("<div id='item-"+id+"' class='lista-productos-seleccionados'><div style='text-align:right;position: relative;'><img class='eliminarlista producto' src='img/eliminar.png' onclick='QuitarListaAlibaba("+producto+")'></div><div class='sx-table-pic'>"+imagen+"</div><div class='s-item__info clearfix'><h3 class='product-title'>"+titulo+"</h3><span class='price_alb'>"+precio+"</span><div class='link' style='display:none;'>"+url+"</div></div><div class='cantidad-producto'><label>Cantidad: </label><input type='number' id='cantidad-"+id+"' min='1' max='999' step='1' value='1'></div></div>");
+	var img = $("#"+id+" .sx-table-pic img").attr('src');
 	
 	var f = new Date();
 	var Fecha = f.getDate() + "-" + (f.getMonth() +1) + "-" + f.getFullYear();
 	db.transaction(function(tx){
 		tx.executeSql('INSERT INTO ListaBD(Usuario,Nombre,Telefono,Email,Mensaje,Tienda,IdProducto,Producto,Imagen,Precio,Enlace,Cantidad,Fecha)VALUES (?,?,?,?,?,?,?,?,?,?,?,?,?)',
-					["","","","","","Alibaba",id,titulo,"",precio,url,"",Fecha],nullHandler,errorHandler);
+					["","","","","","Alibaba",id,titulo,img,precio,url,"",Fecha],nullHandler,errorHandler);
 	});	
 	/*$("#productos-alibaba").html("");
 	jQuery.each( AlibabaList, function( i, valor ) {
@@ -286,12 +289,13 @@ function ListaMadechina(producto){
 	var precio = $("#"+id+" .md-precio").html();
 	var url = $("#"+id+" .link span").html();
 	$("#productos-madechina").append("<div id='item-"+id+"' class='lista-productos-seleccionados'><div style='text-align:right;position: relative;'><img class='eliminarlista producto' src='img/eliminar.png' onclick='QuitarListaMadeChina("+producto+")'></div><div class='sx-table-pic'>"+imagen+"</div><div class='md-texto'><div class='md-titulo'>"+titulo+"</div><span class='md-precio'>"+precio+"</span><div class='link' style='display:none;'>"+url+"</div></div><div class='cantidad-producto'><label>Cantidad: </label><input type='number' id='cantidad-"+id+"' min='1' max='999' step='1' value='1'></div></div>");
+	var img = $("#"+id+" .sx-table-pic img").attr('src');
 	
 	var f = new Date();
 	var Fecha = f.getDate() + "-" + (f.getMonth() +1) + "-" + f.getFullYear();
 	db.transaction(function(tx){
 		tx.executeSql('INSERT INTO ListaBD(Usuario,Nombre,Telefono,Email,Mensaje,Tienda,IdProducto,Producto,Imagen,Precio,Enlace,Cantidad,Fecha)VALUES (?,?,?,?,?,?,?,?,?,?,?,?,?)',
-					["","","","","","MadeChina",id,titulo,"",precio,url,"",Fecha],nullHandler,errorHandler);
+					["","","","","","MadeChina",id,titulo,img,precio,url,"",Fecha],nullHandler,errorHandler);
 	});
 	/*$("#productos-madechina").html("");
 	jQuery.each( MadechinaList, function( i, valor ) {
@@ -367,12 +371,13 @@ function ListaAliexpress(producto){
 	var precio = $("#"+id+" .ali-precio").html();
 	var url = $("#"+id+" .link span").html();
 	$("#productos-aliexpress").append("<div id='item-"+id+"' class='lista-productos-seleccionados'><div style='text-align:right;position: relative;'><img class='eliminarlista producto' src='img/eliminar.png' onclick='QuitarListaAliexpress("+producto+")'></div><div class='sx-table-pic'>"+imagen+"</div><div class='ali-texto'><h3 class='ali-titulo'>"+titulo+"</h3><span class='ali-precio'>"+precio+"</span><div class='link' style='display:none;'>"+url+"</div></div><div class='cantidad-producto'><label>Cantidad: </label><input type='number' id='cantidad-"+id+"' min='1' max='999' step='1' value='1'></div></div>");
+	var img = $("#"+id+" .sx-table-pic img").attr('src');
 	
 	var f = new Date();
 	var Fecha = f.getDate() + "-" + (f.getMonth() +1) + "-" + f.getFullYear();
 	db.transaction(function(tx){
 		tx.executeSql('INSERT INTO ListaBD(Usuario,Nombre,Telefono,Email,Mensaje,Tienda,IdProducto,Producto,Imagen,Precio,Enlace,Cantidad,Fecha)VALUES (?,?,?,?,?,?,?,?,?,?,?,?,?)',
-					["","","","","","Aliexpress",id,titulo,"",precio,url,"",Fecha],nullHandler,errorHandler);
+					["","","","","","Aliexpress",id,titulo,img,precio,url,"",Fecha],nullHandler,errorHandler);
 	});	
 	/*$("#productos-aliexpress").html("");
 	jQuery.each( AliexpressList, function( i, valor ) {
